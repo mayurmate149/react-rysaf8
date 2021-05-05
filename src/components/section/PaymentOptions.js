4917c33e7f964e54d4b3c894fb314f7dabb9b251
mayurmate149 <webguy18@gmail.com>
2021-05-05 17:32:14 +0530

    Show monthly and one time pay in option component.

--- src/components/section/PaymentOptions.js
+++ src/components/section/PaymentOptions.js
@@ -2,15 +2,62 @@ import React, { useState, useContext } from "react";
 import { DataContext } from "../Context";
 import "../css/PaymentOptions.css";
 
+const FullPayInDetails = amount => {
+  const { price } = amount;
+  return (
+    <div className="full-pay-in">
+      <div className="one-time-payment">
+        <div>one-timepayment</div>
+        <div className="price">$ {price}</div>
+      </div>
+    </div>
+  );
+};
+const MonthlyPayInDetails = amount => {
+  const { price } = amount;
+  return (
+    <div className="full-pay-in">
+      <div className="one-time-payment">
+        <div>Monthly payments</div>
+        <div className="price">$ {price}</div>
+      </div>
+    </div>
+  );
+};
+
 const PaymentOptions = function(price) {
   // const { selectedColor, updateSelectedColor } = useContext(DataContext);
+  const [fullPayInActive, setFullPayInActive] = useState(false);
+  const [monthlyPayInActive, setmonthlyPayInActive] = useState(false);
+
+  const onFullPayInClick = () => {
+    setFullPayInActive(true);
+    setmonthlyPayInActive(false);
+  };
+  const onMonthlyPayInClick = () => {
+    setmonthlyPayInActive(true);
+    setFullPayInActive(false);
+  };
+
   return (
     <div className="mt-md mb-md">
       <h3 className="option-heading">Choose Payment Options</h3>
       <div className="payment-option-container">
-        <div>Pay in full</div>
-        <div>Pay monthly</div>
+        <div
+          className={fullPayInActive ? "active" : ""}
+          onClick={() => onFullPayInClick()}
+        >
+          Pay in full
+        </div>
+        <div
+          className={monthlyPayInActive ? "active" : ""}
+          onClick={() => onMonthlyPayInClick()}
+        >
+          Pay monthly
+        </div>
       </div>
+      {fullPayInActive && <FullPayInDetails {...price} />}
+      {monthlyPayInActive && <MonthlyPayInDetails {...price} />}
     </div>
   );
 };
